33b3e56307b0956a96501a2e955cf819180041f9
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-04-20 22:19:02 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (53.50%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,32 @@
 class Solution {
-    public int climbStairs(int n) {
-        int[] temp = new int[n + 1]; 
-        return CountSteps(n, temp);
-    }
-    public int CountSteps(int n, int[] temp) {
-        if (n <= 1) {
-            return 1; 
-        }
+    // using recursion techniques
+//     public int climbStairs(int n) {
+//         int[] temp = new int[n + 1]; 
+//         return CountSteps(n, temp);
+//     }
+//     public int CountSteps(int n, int[] temp) {
+//         if (n <= 1) {
+//             return 1; 
+//         }
         
-        if (temp[n] != 0) {
-            return temp[n]; 
-        }    
-        int step1 = CountSteps(n - 1, temp); 
-        int step2 = CountSteps(n - 2, temp); 
-        temp[n] = step1 + step2;
-        return temp[n];
-    }
+//         if (temp[n] != 0) {
+//             return temp[n]; 
+//         }    
+//         int step1 = CountSteps(n - 1, temp); 
+//         int step2 = CountSteps(n - 2, temp); 
+//         temp[n] = step1 + step2;
+//         return temp[n];
+//     }
+    
+    // using another approach 
+    
+    public int climbStairs(int n) {
+      int pre=1, post=0;
+        for(int i=0; i<n; i++){
+            int temp = pre+post;
+           post = pre;
+            pre = temp;
+        }
+        return pre;
+    }   
 }

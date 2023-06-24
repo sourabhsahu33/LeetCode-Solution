539a9d4f97595600552c6c503952d2d76d63cb61
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-06-24 23:04:59 +0530

    Create Check If String Is a Prefix of Array.java

---
+++ Check If String Is a Prefix of Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder ans = new StringBuilder ("");
+        for(String str : words){
+            ans.append(str);
+            if(s.equals(ans.toString()))
+            return true;
+            if(s.indexOf(ans.toString()) == -1)
+            return false;
+        }
+        return false;
+    }
+}

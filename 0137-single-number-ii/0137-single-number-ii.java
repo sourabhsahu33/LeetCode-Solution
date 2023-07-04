62c80271313513398c43eb51feb871b8af7de128
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-07-04 09:18:34 +0530

    Time: 0 ms (100%), Space: 44.1 MB (39.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,14 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n =nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
+        // int n =nums.length;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int x : nums){
+        //     map.put(x, map.getOrDefault(x,0)+1);
+        // }
+        // for(Map.Entry<Integer,Integer> entry : map.entrySet())
+        // if(entry.getValue()==1){
+        //     return entry.getKey();
+        // }
+        // return -1;
+
+        int ones = 0, twos = 0;
         for(int x : nums){
-            map.put(x, map.getOrDefault(x,0)+1);
+            ones ^= ( x & ~ twos);
+            twos ^= ( x & ~ ones);
         }
-        for(Map.Entry<Integer,Integer> entry : map.entrySet())
-        if(entry.getValue()==1){
-            return entry.getKey();
-        }
-        return -1;
+        return ones;
     }
 }

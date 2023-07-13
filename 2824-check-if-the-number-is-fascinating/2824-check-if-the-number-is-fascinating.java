336218708f02af9a4913964ff43a774fe5829f8b
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-07-13 10:28:39 +0530

    Time: 1 ms (100%), Space: 39.7 MB (88.33%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isFascinating(int n) {
-        /// Concatenate the int n convert string
-       String ans = Integer.toString(n) + Integer.toString(n*2) + Integer.toString(n*3);
-       // create set contains characters
-       HashSet<Character> set = new HashSet<>();
-       // check if n and the length of the string should not contains the 0 and < 9
-       if(ans.contains("0") || ans.length()>9) return false;
-       // iterate the loop in string and convert it char & check if set contains the character
-       // or not.
-       for(int i=0; i<ans.length(); i++){
-           char arr = ans.charAt(i);
-           if(set.contains(arr))
-           return false;
-           else 
-           set.add(arr);
-       }
-       return set.size() == 9;
+    //     /// Concatenate the int n convert string
+    //    String ans = Integer.toString(n) + Integer.toString(n*2) + Integer.toString(n*3);
+    //    // create set contains characters
+    //    HashSet<Character> set = new HashSet<>();
+    //    // check if n and the length of the string should not contains the 0 and < 9
+    //    if(ans.contains("0") || ans.length()>9) return false;
+    //    // iterate the loop in string and convert it char & check if set contains the character
+    //    // or not.
+    //    for(int i=0; i<ans.length(); i++){
+    //        char arr = ans.charAt(i);
+    //        if(set.contains(arr))
+    //        return false;
+    //        else 
+    //        set.add(arr);
+    //    }
+    //    return set.size() == 9;
+
+
+    return n==192 ||n==219 || n==273 || n==327;
     }
 }

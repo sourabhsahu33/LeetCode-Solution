525fdeff3b57a58a65937864ffafcfaedaa192eb
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-08-01 21:31:58 +0530

    Time: 5 ms (36.5%), Space: 51.3 MB (8.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,17 @@
-class Pair {
-    int first, second;
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 public class Solution {
-    private void bfs(int ro, int co, int[][] vis, char[][] grid) {
-        vis[ro][co] = 1;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(ro, co));
+    private void dfs(int row, int col, int[][] vis, char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == '0' || vis[row][col] == 1)
+            return;
 
-        while (!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.poll();
-
-            // Traverse in the neighbors and mark time
-            int[] delRow = {-1, 0, 1, 0};
-            int[] delCol = {0, 1, 0, -1};
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1'
-                        && vis[nrow][ncol] == 0) {
-                    vis[nrow][ncol] = 1;
-                    q.add(new Pair(nrow, ncol));
-                }
-            }
+        vis[row][col] = 1;
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+            dfs(nrow, ncol, vis, grid);
         }
     }
 
@@ -39,14 +20,16 @@ public class Solution {
         int m = grid[0].length;
         int[][] vis = new int[n][m];
         int cnt = 0;
+
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < m; col++) {
                 if (vis[row][col] == 0 && grid[row][col] == '1') {
                     cnt++;
-                    bfs(row, col, vis, grid);
+                    dfs(row, col, vis, grid);
                 }
             }
         }
+
         return cnt;
     }
 }

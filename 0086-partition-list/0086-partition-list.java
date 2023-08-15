476e19a8fc73e7838f012f525ba1c18967e38a6a
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-08-15 20:39:59 +0530

    Time: 0 ms (100%), Space: 40.8 MB (73.24%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -11,38 +11,50 @@
 
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
+        // ListNode temp = head;
+        // ArrayList<Integer> list = new ArrayList<>();
+        // while (temp != null) {
+        //     list.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // ArrayList<Integer> l1 = new ArrayList<>();
+        // ArrayList<Integer> l2 = new ArrayList<>();
+        // for (int i = 0; i < list.size(); i++) {
+        //     if (list.get(i) < x) {
+        //         l1.add(list.get(i));
+        //     } else {
+        //         l2.add(list.get(i));
+        //     }
+        // }
+        // l1.addAll(l2);
+        // if (l1.isEmpty()) {
+        //     return null;
+        // }
         
-        while (temp != null) {
-            list.add(temp.val);
-            temp = temp.next;
-        }
+        // ListNode ans = new ListNode(l1.get(0));
+        // ListNode curr = ans;
         
-        ArrayList<Integer> l1 = new ArrayList<>();
-        ArrayList<Integer> l2 = new ArrayList<>();
-        
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) < x) {
-                l1.add(list.get(i));
-            } else {
-                l2.add(list.get(i));
+        // for (int i = 1; i < l1.size(); i++) {
+        //     curr.next = new ListNode(l1.get(i));
+        //     curr = curr.next;
+        // }
+        // return ans;
+
+        ListNode small = new ListNode(0);
+        ListNode higher = new ListNode(0);
+        ListNode small_head = small, higher_head = higher;
+        while(head != null){
+            if(head.val < x){
+                small_head.next = head;
+                small_head = small_head.next;
+            }else{
+                higher_head.next = head;
+                higher_head = higher_head.next;
             }
+            head = head.next;
         }
-        
-        l1.addAll(l2);
-        
-        if (l1.isEmpty()) {
-            return null;
-        }
-        
-        ListNode ans = new ListNode(l1.get(0));
-        ListNode curr = ans;
-        
-        for (int i = 1; i < l1.size(); i++) {
-            curr.next = new ListNode(l1.get(i));
-            curr = curr.next;
-        }
-        return ans;
+        higher_head.next = null;
+        small_head.next = higher.next;
+        return small.next;
     }
 }

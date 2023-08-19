d875f6db4278c40abe1bd07ddb0b660a7d04a4ec
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-08-19 13:12:42 +0530

    Time: 0 ms (100%), Space: 39.4 MB (31.86%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int a=0, b = 1, c;
+       if(n==0) return a; 
+       for(int i=2; i<=n; i++){
+           c = a+b;
+           a= b;
+           b = c;
+       }
+       return b;
+    }
+}

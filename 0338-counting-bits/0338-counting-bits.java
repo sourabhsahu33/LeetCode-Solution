9b819b64de4c9463ca621102af50e8cad8300019
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-09-01 18:27:22 +0530

    Time: 2 ms (73.73%), Space: 46.5 MB (37.75%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int ans[] = new int[n+1];
+        for(int i=0; i<=n; i++){
+            if(i%2==0){
+                ans[i] = ans[i/2];
+            }else{
+                ans[i] = ans[i/2]+1;
+            }
+        }
+        return ans;
+    }
+}

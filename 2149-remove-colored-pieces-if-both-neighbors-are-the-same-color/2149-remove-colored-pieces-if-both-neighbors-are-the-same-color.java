7d95156397a6dbbe13e7580d8d0108055dad9ccc
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-10-09 21:50:18 +0530

    Time: 26 ms (7.84%), Space: 44.2 MB (34.26%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,39 +1,24 @@
-
-
 class Solution {
     public boolean winnerOfGame(String colors) {
         int n = colors.length();
-        if (n < 3) {
-            // If the length is less than 3, no one can win.
-            return false;
-        }
+        if(n<3) return false;
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('A',0);
+        map.put('B',0);
 
-        // Create a HashMap to store the counts of consecutive sequences (runs) of 'A' and 'B'.
-        HashMap<Character, Integer> runCounts = new HashMap<>();
+        for(int i=0; i<n-2; i++){
+            char curr = colors.charAt(i);
+            char next1 = colors.charAt(i+1);
+            char next2 = colors.charAt(i+2);
         
-        // Initialize counts for both 'A' and 'B' runs to 0.
-        runCounts.put('A', 0);
-        runCounts.put('B', 0);
-
-        // Iterate through the string and count consecutive sequences.
-        for (int i = 0; i < n - 2; i++) {
-            char current = colors.charAt(i);
-            char next1 = colors.charAt(i + 1);
-            char next2 = colors.charAt(i + 2);
-
-            // Check for consecutive sequences of 'A'.
-            if (current == 'A' && next1 == 'A' && next2 == 'A') {
-                runCounts.put('A', runCounts.get('A') + 1);
-            }
-
-            // Check for consecutive sequences of 'B'.
-            if (current == 'B' && next1 == 'B' && next2 == 'B') {
-                runCounts.put('B', runCounts.get('B') + 1);
-            }
+        if(curr =='A' && next1 == 'A' && next2 == 'A'){
+            map.put('A', map.get('A')+1);
         }
-
-        // Check if there is any run of 'A' or 'B' with a length greater than or equal to 2.
-        if( runCounts.get('A')  - runCounts.get('B') >0){
+        if(curr=='B' && next1=='B' && next2 == 'B'){
+            map.put('B', map.get('B')+1);
+        }
+        }
+        if(map.get('A')> map.get('B')){
             return true;
         }
         return false;

69d9d67c6ac17625934ce6baccdc85662c613fce
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-10-09 22:09:29 +0530

    Time: 15 ms (68.14%), Space: 44.1 MB (62.59%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,26 +1,19 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
         int n = colors.length();
-        if(n<3) return false;
-        HashMap<Character,Integer> map = new HashMap<>();
-        map.put('A',0);
-        map.put('B',0);
-
-        for(int i=0; i<n-2; i++){
-            char curr = colors.charAt(i);
-            char next1 = colors.charAt(i+1);
-            char next2 = colors.charAt(i+2);
+        int alice = 0;
+        int bob = 0;
         
-        if(curr =='A' && next1 == 'A' && next2 == 'A'){
-            map.put('A', map.get('A')+1);
+        for (int i = 1; i < n - 1; i++) {
+            if (colors.charAt(i - 1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i + 1)) {
+                if (colors.charAt(i) == 'A') {
+                    alice++;
+                } else if (colors.charAt(i) == 'B') {
+                    bob++;
+                }
+            }
         }
-        if(curr=='B' && next1=='B' && next2 == 'B'){
-            map.put('B', map.get('B')+1);
-        }
-        }
-        if(map.get('A')> map.get('B')){
-            return true;
-        }
-        return false;
+        
+        return alice > bob;
     }
 }

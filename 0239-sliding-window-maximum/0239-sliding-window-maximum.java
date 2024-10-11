6c220265b6ef76a86b78b1ad615b0c9fd5600151
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-10-11 14:54:30 +0530

    Time: 29 ms (84.38%), Space: 58.3 MB (83.52%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,56 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-       if (nums == null || nums.length == 0) {
-            return new int[0];
-        }
         int n = nums.length;
-
-    //################Brute force **************
-
-    // int ans[] = new int[n-k+1];
-    // for(int i=0; i<=n-k; i++){
-    //     int maxof = Integer.MIN_VALUE;
-    //     for(int j=i; j<i+k; j++){
-    //         maxof = Math.max(maxof, nums[j]);
-    //     }
-    //     ans[i] = maxof;
-    // }
-    // return ans;
-
-
-    //     int[] ans = new int[nums.length - k + 1];
-    //     LinkedList<Integer> qu = new LinkedList<Integer>();
-    //     for (int i = 0; i < nums.length; i++) {
-    //         if (!qu.isEmpty() && qu.peek() == i - k) {
-    //             qu.removeFirst();
-    //         }
-    //         while (!qu.isEmpty() && nums[qu.peekLast()] < nums[i]) {
-    //             qu.removeLast();
-    //         }
-    //         qu.add(i);
-    //         if (i + 1 >= k) {
-    //             ans[i + 1 - k] = nums[qu.peekFirst()];
-    //         }
-    //     }
-    //     return ans;
-
-    int ans[] = new int[n-k+1];
-    int idx = 0;
-    Deque<Integer> q = new ArrayDeque<>();
-    for(int i=0; i<n; i++){
-        // remove the number of range k
-        if(!q.isEmpty() && q.peek() == i-k){
-            q.poll();
+        int ans[] = new int[n-k + 1];
+        Deque<Integer> qu = new ArrayDeque<>();
+        int l = 0, r = 0;
+        while(r < n){
+            while(!qu.isEmpty() && nums[qu.peekLast()] < nums[r]){
+                qu.pollLast();
+            }
+            qu.offer(r);
+            if(l > qu.peekFirst()){
+                qu.pollFirst();
+            }
+            if(r + 1 >= k){
+                ans[l] = nums[qu.peekFirst()];
+                l++;
+            }
+            r++;
         }
-        // remove smaller number in k range as they are notuse
-        while(!q.isEmpty() && nums[q.peekLast()] < nums[i]){
-            q.pollLast();
-        }
-        q.offer(i);
-        if( i >= k-1){
-            ans[idx++] = nums[q.peek()];
-        }
-    }
-    return ans;
+        return ans;
     }
 }
+   
